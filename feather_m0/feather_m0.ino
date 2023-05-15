6af320b89d1ceac9c7aafdb92e06ebe0669b201a
Connor Forbes <connorforbes25@gmail.com>
2023-05-15 10:06:36 +1000

    fix: Number of retries comparator

--- feather_m0/feather_m0.ino
+++ feather_m0/feather_m0.ino
@@ -96,7 +96,7 @@ void loop()
     // Turn off sensor
     digitalWrite(SHT_PWD_PIN, LOW);
     // Send packet to gateway if sensor reading exists
-    if (num_retries <= 10) {
+    if (num_retries < 10) {
       radio.sendPacket(temperature.temperature, humidity.relative_humidity, sensor_id);
       delay(10); // This is needed to prevent hanging
       Serial.println("Packet sent!");
